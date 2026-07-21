072a3047d107afc3b3d87e93dc618b0cd0762481
Antoine Glasson <ant.glasson@gmail.com>
2026-07-21 10:24:34 +0100

    fix: remove preproc

--- src/main.rs
+++ src/main.rs
@@ -9,11 +9,6 @@ use clap::Parser;
 
 #[derive(Parser, Debug)]
 #[command(version, about)]
-#[command(group(
-    clap::ArgGroup::new("mode")
-        .required(true)
-        .args(["nav", "preproc"]),
-))]
 struct Args {
     /// Path to configuration file
     #[arg(long, default_value = "config/default.toml")]
@@ -22,16 +17,9 @@ struct Args {
     /// Launch navigation mode with the input inside given folder.
     /// The folder must contain:
     /// semantic.txt (Scenescript output),
-    ///  tags_world_config.json (can be computed using --preproc)
+    /// tags_world_config.json
     #[arg(long)]
     nav: Option<String>,
-
-    /// Launch preprocessing mode with the input inside the given folder.
-    /// The folder must contain:
-    /// video.mp4 (Extracted from .vrs),
-    ///  closed_loop_trajectory.csv (Extracted from .vrs using MPS)
-    #[arg(long)]
-    preproc: Option<String>,
 }
 
 fn main() -> anyhow::Result<()> {
@@ -47,10 +35,8 @@ fn main() -> anyhow::Result<()> {
     let config_path = args.config;
     let config = config::load_config(config_path)?;
     if let Some(nav_path) = args.nav {
-        println!("Launching navigation mode: {}", nav_path);
+        println!("Launching navigation : {}", nav_path);
         sina::Sina::new().launch(&config, nav_path, (3.0, 5.0))?;
-    } else if let Some(preproc_path) = args.preproc {
-        println!("Launching preprocessing: {}", preproc_path);
     }
 
     Ok(())
